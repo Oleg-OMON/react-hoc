e03cd139b8d82fa53b42754cd0e00fb0c829dba7
Oleg Zasedatelew <ol.zasedatelew@yandex.ru>
2023-01-14 13:26:06 +0300

    first commit

---
+++ src/components/Sections/SectionListItem.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { DateTime, Video } from "../Atoms/Atoms";
+import witchDateTimePretty from "../HOCs/DateTimePretty";
+import PropTypes from "prop-types";
+
+
+
+const SectionListItem = (props) => {
+  const {USID} = require("usid");
+  const usid = new USID();
+  const DataTimePretty = witchDateTimePretty(DateTime);
+
+  console.log(props, 'propsItem')
+  return (
+    <li
+      key={usid.rand()}
+      data-id={props.id}
+      className={props.className + "-item"}
+    >
+      <div key={usid.rand()} className={props.className + "-content"}>
+        <Video
+          key={usid.rand()}
+          {...props}
+          className={props.className + " video"}
+        ></Video>
+        <DataTimePretty
+          key={usid.rand()}
+          {...props}
+          text={props.dateTime}
+          className={props.className}
+        ></DataTimePretty>
+      </div>
+    </li>
+  );
+};
+
+SectionListItem.propTypes = {
+  props: PropTypes.object
+};
+
+export default SectionListItem;
